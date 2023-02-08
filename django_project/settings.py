ae4191f494222c4f886b887d60ba4cdc19852ec9
dungnqen <121447177+dungnqen@users.noreply.github.com>
2023-02-08 21:26:50 +0700

    finalizing

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1e3907af400969d58dee294d74bf1620'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'sonhanh.vn', '139.59.121.0', 'www.sonhanh.vn', '127.0.0.1', 'vattutot.com.vn', 'www.vattutot.com.vn', 'quangdung.shop']
+ALLOWED_HOSTS = ['localhost', 'sonhanh.vn', '139.59.121.0', 'www.sonhanh.vn', '127.0.0.1', 'vattutot.com.vn', 'www.vattutot.com.vn', 'quangdung.shop', 'www.quangdung.shop']
 # ALLOWED_HOSTS = []
 
 # Application definition
@@ -129,6 +129,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = ['https://vattutot.com.vn','https://www.vattutot.com.vn']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
--- django_project/views.py
+++ django_project/views.py
@@ -4,7 +4,6 @@ from .models import Item
 
 
 def index(request):
-  # return render(request, "index.html")
-  return render(request, "index2.html", {
+  return render(request, "index.html", {
     "items": Item.objects.all()
   })
