8d009be16c5a7fe9af7fbc1875fbba1d5fb82540
dungnqen <121447177+dungnqen@users.noreply.github.com>
2023-02-09 18:29:36 +0700

    add_item_view

--- django_project/views.py
+++ django_project/views.py
@@ -7,3 +7,10 @@ def index(request):
   return render(request, "index.html", {
     "items": Item.objects.all()
   })
+
+def item_view(request, item_path):
+  item = Item.objects.get(url_path=item_path)
+  return render(request, "item_view.html", {
+    "item": item
+  })
+
